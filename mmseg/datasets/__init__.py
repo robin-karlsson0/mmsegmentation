279f0923c5f21ffd1b7cde5b324d46274dc524a8
Robin Karlsson <robin.karlsson0@gmail.com>
2021-10-09 18:29:33 +0900

    ADD NTHU dataset classes

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -11,6 +11,7 @@ from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .drive import DRIVEDataset
 from .hrf import HRFDataset
 from .night_driving import NightDrivingDataset
+from .nthu import NTHUDataset
 from .pascal_context import PascalContextDataset, PascalContextDataset59
 from .stare import STAREDataset
 from .voc import PascalVOCDataset
@@ -21,5 +22,5 @@ __all__ = [
     'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
     'PascalContextDataset59', 'ChaseDB1Dataset', 'DRIVEDataset', 'HRFDataset',
     'STAREDataset', 'DarkZurichDataset', 'NightDrivingDataset',
-    'COCOStuffDataset', 'A2D2Dataset'
+    'COCOStuffDataset', 'A2D2Dataset', 'NTHUDataset'
 ]
