d4de4b73a44b43f020bbd16c7728f21604802e24
Robin Karlsson <robin.karlsson0@gmail.com>
2021-10-12 16:59:45 +0900

    ADD GTA 5 VISSL finetune config

---
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1052_40k_gta5_vissl_finetune.py
@@ -0,0 +1,9 @@
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8_vissl_finetune.py',
+    '../_base_/datasets/gta5.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_40k.py'
+]
+
+# Layer group specific LR multipliers
+optimizer = dict(
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}))
