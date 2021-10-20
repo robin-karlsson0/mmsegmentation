dd9bbafd119a8937ddc513d251a7a77287e97c5a
Robin Karlsson <robin.karlsson0@gmail.com>
2021-10-20 23:44:38 +0900

    ADD Small FCN task heads for VISSL models

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -3,8 +3,9 @@ from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
 from .encoder_decoder_vissl import EncoderDecoderVISSL
+from .encoder_decoder_vissl_fcn import EncoderDecoderVISSLFCN
 
 __all__ = [
     'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder',
-    'EncoderDecoderVISSL'
+    'EncoderDecoderVISSL', 'EncoderDecoderVISSLFCN'
 ]
