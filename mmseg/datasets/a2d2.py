0d16b8a4751568f25696efe587b95b866a602c17
Robin Karlsson <robin.karlsson0@gmail.com>
2022-03-24 18:34:21 +0900

    ADD A2D2 road markings

--- mmseg/datasets/a2d2.py
+++ mmseg/datasets/a2d2.py
@@ -6,7 +6,7 @@ from .custom import CustomDataset
 
 
 @DATASETS.register_module()
-class A2D2Dataset(CustomDataset):
+class A2D2MarkingsDataset(CustomDataset):
     """A2D2 dataset following the Cityscapes 'trainids' label format.
 
     The dataset features 41,280 frames with semantic segmentation in 38
@@ -28,18 +28,13 @@ class A2D2Dataset(CustomDataset):
     Ref: https://www.a2d2.audi/a2d2/en/dataset.html
     """
 
-    CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
-               'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
-               'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
-               'bicycle')
+    CLASSES = ('road', 'markings')
 
-    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
-               [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
-               [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
-               [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
-               [0, 80, 100], [0, 0, 230], [119, 11, 32]]
+    PALETTE = [[0, 0, 0], [255, 255, 255]]
 
     def __init__(self, **kwargs):
-        super(A2D2Dataset, self).__init__(
-            img_suffix='.png', seg_map_suffix='_labelTrainIds.png', **kwargs)
+        super(A2D2MarkingsDataset, self).__init__(
+            img_suffix='.png',
+            seg_map_suffix='_markingsTrainIds.png',
+            **kwargs)
         assert osp.exists(self.img_dir) is not None
